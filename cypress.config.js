de2efa4ded144a3e3b113aa7b343db26bc308d88
hhhameem <hebronhossainhamim@gmail.com>
2024-04-01 20:45:54 +0600

    ✅ added the order of the test execution

--- cypress.config.js
+++ cypress.config.js
@@ -4,7 +4,16 @@ module.exports = defineConfig({
   e2e: {
     baseUrl: "https://restful-booker.herokuapp.com",
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      config.specPattern = [
+        "cypress/e2e/auth.cy.js",
+        "cypress/e2e/getBookingIds.cy.js",
+        "cypress/e2e/getBooking.cy.js",
+        "cypress/e2e/createBooking.cy.js",
+        "cypress/e2e/updateBooking.cy.js",
+        "cypress/e2e/partialUpdateBooking.cy.js",
+        "cypress/e2e/deleteBooking.cy.js",
+      ];
+      return config;
     },
   },
 });
